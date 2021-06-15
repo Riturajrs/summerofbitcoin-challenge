c496fd5ec2485f07eba42099d92d5a297cac2f4e
Rituraj Seal <rituraj2001.rs@gmail.com>
2021-06-15 15:23:50 +0530

    commiting files

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ I have used greedy algorithim for maximising the profit, which means<br>
 that solution produced will not be perfect but it will be close to perfect.<br>
 A better approach would be applying dynamic programming to solve it.<br>
 <br>
-Please contribute if you can.<br>
-Thank you.
+Total transactions : 5214<br>
+Total weight : 1769100<br>
+Total profit : 1078091 
---
+++ source.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define nl "\n" 
+using namespace std;
+
+struct coins{
+    string txid;
+    vector<string> ptxid;
+    ll weight;
+    ll fee;
+    int f;
+    ll total_fee;
+    ll total_weight;
+};
+
+// txid,weight,parent_txids,fee,total_fee
+vector<coins> id;
+vector<string> sol;
+ll weight = 0;
+// Converting string to integer
+ll stringTointeger(string str)
+{
+    ll temp = 0;
+    for (ll i = 0; i < str.length(); i++) {
+        temp = temp * 10 + (str[i] - '0');
+    }
+    return temp;
+}
+bool compareThem(const coins &a,const coins &b){
+    if( (a.total_fee)*(b.total_weight) >= (b.total_fee)*(a.total_weight) )
+    return true;
+    else 
+    return false;
+}
+void solve(coins a ){
+    weight += a.weight;
+    if(a.ptxid.empty() || weight > 4000000 )
+        return ;
+    for( auto it : a.ptxid ){
+        for( auto it1 : id ){
+            if( it1.txid == it ){
+                solve(it1);
+                sol.push_back(it1.txid);
+            }
+        }
+    }
+
+}
+int main(){
+    fstream fin;
+    fin.open("mempool.csv",ios::in);
+    string temp,s1,s2,s3;
+    ll i,j;
+    coins temp1;
+    // Parsing the CSV file
+    // in the form of a graph
+    fin >> temp;
+    while( fin >> temp ){
+        temp += ", ";
+        int c = 0;
+        vector<string> v;
+        for( i = 0 ; i < temp.size() ; i++ ){
+            if( temp[i] == ','){
+                if( c == 0 ){
+                    temp1.txid = s1;
+                }
+                else if( c == 1 ){
+                    temp1.fee = stringTointeger(s1);
+                }
+                else if( c == 2 ){
+                    temp1.weight = stringTointeger(s1);
+                }
+                else if( c == 3 ){
+                    s1 += ";";
+                    for( j = 0 ; j < s1.size() ; j++ ){
+                        if( s1[j] == ';' ){
+                            v.push_back(s2);
+                            s2 = "";
+                        }
+                        else{
+                            s2 += s1[j];
+                        }
+                    }
+                    i = s1.size();
+                }
+                s1 = "";
+                i++;
+                c++;
+            }
+            else{
+                s1 += temp[i];
+            }
+        }
+       if( temp1.ptxid.size() > 0 ){
+        temp1.f = 1;
+        }
+        else{
+        temp1.f = 0;
+        }
+        temp1.total_fee = 0;
+        temp1.total_weight = 0;
+        v.clear();
+        id.push_back(temp1);
+    }
+    int f;
+    // Calculating total fees for every transaction using breadth first search
+    for( i = 0 ; i < id.size() ; i++ ){
+        queue<coins> q;
+        q.push(id[i]);
+        while( !q.empty() ){
+            id[i].total_fee += q.front().fee;
+            id[i].total_weight += q.front().weight;
+            for( auto it : q.front().ptxid ){
+                f = 0;
+                for( j = 0 ; j < i ; j++ ){
+                    if( id[j].txid == it ){
+                        q.push(id[j]);
+                        f = 1;
+                        break;
+                    }
+                }
+                if( f == 0 ){
+                    while( q.size() > 1 ){
+                        q.pop();
+                    }
+                    id[i].f = 0;
+                    break;
+                }
+            }
+            q.pop();
+        }
+    }
+    //Sorting transaction using Greedy algortihim
+    // sort(txid.begin(),txid.end(),compareThem);
+    //Storing the transaction ids
+    for( auto it : id ){
+        if( weight + it.weight > 4000000 ){
+            break;
+        }
+        if( it.f )
+        solve(it);
+        sol.push_back(it.txid);
+        weight += it.weight;
+    }
+    // Creating block.txt file
+    ofstream file;
+    file.open("block.txt");
+    // Writing in block.txt file
+    for( i = 0 ; i < sol.size() ; i++ ){
+        f = 0;
+        for( j = 0 ; j < i ; j++ ){
+            if( sol[i] == sol[j] ){
+                f++;
+                break;
+            }
+        }
+        if( f == 0 ){
+            file << sol[i] << nl;
+        }
+    }
+    file.close();
+    return 0;
+}
